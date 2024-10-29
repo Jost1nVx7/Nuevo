f882f2b4a64c62a63348cc8e845824e25eb38057
Jost1nVx7 <jostinjd2002@gmail.com>
2024-10-28 20:47:29 -0500

    commit

---
+++ src/main/java/espoch/edu/ec/calculadora/Calculadora.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package espoch.edu.ec.calculadora;
+
+  import java.util.Scanner;
+/**
+ *
+ * @author Global Technology Ec
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        
+        Scanner Yape= new Scanner(System.in);
+        System.out.println("ingresa el primer numero:");
+        int num1=Yape.nextInt();
+        
+        System.out.println("ingresa el segundo numero:");
+        int num2=Yape.nextInt();
+        
+        int suma=num1+num2;
+        
+        System.out.println("la suma es "+suma);
+        
+
+        
+
+        //int[] enteros;
+
+        // inicializar vector
+        //enteros = new int[3];
+       // int [] enteros={5,7,9};
+  
+        //enteros[0] = 5;
+        //enteros[1] = 7;
+        //enteros[2] = 9;
+
+       // for (int i = 0; i <= 2; i++) {
+         //System.out.println("los valores del vector son: " + enteros[i]);
+           // int j=enteros[1];
+            
+            //System.out.println("el valor de la posicion 1 es:"+j);
+         /*   int [] [] enteros;
+           // enteros = new int [3][3];
+            
+            //enteros[0][0]=15;
+            //enteros[1][1]=17;
+            //enteros[2][2]=19;
+            
+            for (int i = 0; i <= 2; i++){
+                for (int j = 0; j <= 2; j++)
+                System.out.println("el valor:"+enteros[i][j]);
+*/
+            
+            }
+        }
+    
+
